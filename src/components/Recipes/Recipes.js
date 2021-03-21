5b11d9d7baa704274b47f6b86db3e44592b85149
hassansari4a <59260934+hassansari4a@users.noreply.github.com>
2021-03-21 16:00:30 +0545

    Commit

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -4,9 +4,9 @@ import Recipe from './Recipe/Recipe'
 function Recipes() {
   return (
     <>
-    <h1>Recipes</h1>
-    <Recipe />
-    <Recipe />
+      <h1>Recipes</h1>
+      <Recipe />
+      <Recipe />
     </>
   )
 }
