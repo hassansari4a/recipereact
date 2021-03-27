c329f9d31c5d34a27ace1467d13a8a82e0a00d9d
hassansari4a <59260934+hassansari4a@users.noreply.github.com>
2021-03-27 16:43:35 +0545

    added dummy data

--- src/data.js
+++ src/data.js
@@ -24,15 +24,15 @@
   dishname: 'Olive Sandwich',
   ingredients: [{
     ingredientname : 'Bread',
-    quantity : '2',
-    unit : ''
-  },{
-    ingredientname : 'Olives',
     quantity : '10',
     unit : ''
   },{
-    ingredientname : 'Banana',
+    ingredientname : 'Olives',
     quantity : '2',
+    unit : ''
+  },{
+    ingredientname : 'Banana',
+    quantity : '6',
     unit : ''
   },{
     ingredientname : 'Maple Syrup',
@@ -40,8 +40,52 @@
     unit : ''
   }
   ],
-  steps: 'Get two Breads\nCut your bananas\nDrench in Maple syrup\nSpread olives all over',
+  steps: 'Get two Breads\nCut your bananas\nDrench in Maple syrup\nSpread olives all over\nEat and be happy',
   image: 'https://images.food52.com/K3kUw0YWI9wS3-8p1MUaBCBgNtc=/1200x675/cab1b397-7ed6-4bb5-9ad2-589a85b2e82f--oil-cured-olive-sandwich_food52_mark_weinberg_14-09-02_0233.jpg'
+}, {
+  dishname: 'Egg Sandwich, probably',
+  ingredients: [{
+    ingredientname : 'egg',
+    quantity : '3',
+    unit : ''
+  },{
+    ingredientname : 'Bread',
+    quantity : '2',
+    unit : ''
+  },{
+    ingredientname : 'Some kind of leaf',
+    quantity : '7',
+    unit : ''
+  }
+  ],
+  steps: 'put it together\neat and be happy',
+  image: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=653&q=80'
+}, {
+  dishname: 'Pasta',
+  ingredients: [{
+    ingredientname : 'Pasta',
+    quantity : '1',
+    unit : ''
+  },{
+    ingredientname : 'Egg',
+    quantity : '2',
+    unit : ''
+  },{
+    ingredientname : 'Tomato',
+    quantity : '5',
+    unit : ''
+  }, {
+    ingredientname : 'Onion',
+    quantity : '1',
+    unit : ''
+  },{
+    ingredientname : 'Egg',
+    quantity : '2',
+    unit : ''
+  }
+  ],
+  steps: 'Leave pasta in a boiling pot for 9 minutes\nFry onions and tomato\nAdd eggs and seasonings\nFry the boiled pasta\nAdd coriander and stuff idk',
+  image: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
 }]
 
 export default defaultRecipe
